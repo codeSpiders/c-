ccb36e68b486b09574b6ba53e6f95d03405314cd
codeSpiders <102961067+codeSpiders@users.noreply.github.com>
2022-06-16 08:43:30 +0800

    09comparison

---
+++ comparison.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+// == != < <= > >=
+int main() {
+	int a = 3;
+	int b = 4;
+	cout << ( a == b ) << endl;
+	cout << ( a != b ) << endl;
+	cout << ( a > b ) << endl;
+	cout << ( a < b ) << endl;
+	cout << ( a >= b ) << endl;
+	cout << ( a <= b ) << endl;	
+	return 0;
+}
