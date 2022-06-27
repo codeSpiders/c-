05ed80f945366ec8151b190d77de6fbb40413bc6
codeSpiders <102961067+codeSpiders@users.noreply.github.com>
2022-06-27 14:16:47 +0800

    07main

---
+++ add.cpp
@@ -0,0 +1,6 @@
+int add2(int x,int y=20,int z=30){
+    return x+y+z;
+}
+int add3(int x,int y=20,int z=30){
+    return x+y+z;
+}
---
+++ func.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int addc(int x,int y){
+    return x+y;
+}
+int subc(int x,int y){
+    return x-y;
+}
